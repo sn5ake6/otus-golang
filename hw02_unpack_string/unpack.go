5ab0faeebffbd5676cbc9e541b8fe1ff3b3037c4
Alexander Maslennikov <sn5ake6@gmail.com>
2022-03-13 23:21:00 +0500

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,47 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	if s == "" {
+		return "", nil
+	}
+
+	firstChar, _ := utf8.DecodeRuneInString(s)
+	if unicode.IsDigit(firstChar) {
+		return "", ErrInvalidString
+	}
+
+	var previousChar rune
+	var b strings.Builder
+
+	for _, char := range s {
+		if unicode.IsDigit(char) {
+			if unicode.IsDigit(previousChar) {
+				return "", ErrInvalidString
+			}
+
+			count, _ := strconv.Atoi(string(char))
+			if count > 0 {
+				repeatedPreviousChar := strings.Repeat(string(previousChar), count)
+				b.WriteString(repeatedPreviousChar)
+			}
+		} else if !unicode.IsDigit(previousChar) && previousChar != 0 {
+			b.WriteRune(previousChar)
+		}
+
+		previousChar = char
+	}
+	if !unicode.IsDigit(previousChar) {
+		b.WriteRune(previousChar)
+	}
+
+	return b.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,10 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "a0b0c0", expected: ""},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "本3", expected: "本本本"},
+		{input: " 2 3", expected: "     "},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -34,7 +38,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "a45"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
