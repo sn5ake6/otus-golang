cf5c910f398eafbaf50e33295fdc6ba8c8323c0a
Alexander Maslennikov <sn5ake6@gmail.com>
2022-05-22 15:34:43 +0500

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	pb "github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +14,55 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	var file *os.File
+	var err error
+
+	file, err = os.Open(fromPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer file.Close()
+
+	var fileInfo os.FileInfo
+	fileInfo, err = file.Stat()
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	fileSize := fileInfo.Size()
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	var countToRead int64
+	if limit > 0 && (offset+limit) < fileSize {
+		countToRead = limit
+	} else {
+		countToRead = fileSize - offset
+	}
+
+	if offset > 0 {
+		file.Seek(offset, io.SeekStart)
+	}
+
+	reader := io.LimitReader(file, countToRead)
+
+	bar := pb.Full.Start64(countToRead)
+	barReader := bar.NewProxyReader(reader)
+
+	var fileTo *os.File
+	fileTo, err = os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer fileTo.Close()
+
+	_, err = io.Copy(fileTo, barReader)
+	if err != nil {
+		return err
+	}
+
+	bar.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,111 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	fromPath = "testdata/input.txt"
+	tempPath = os.TempDir() + "/hw07_tmp.out"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		var file *os.File
+		file, _ = os.Open(fromPath)
+		defer file.Close()
+
+		var fileInfo os.FileInfo
+		fileInfo, _ = file.Stat()
+
+		fileSize := fileInfo.Size()
+		fileSize++
+
+		err := Copy(fromPath, "", fileSize, 0)
+
+		require.Equal(t, ErrOffsetExceedsFileSize, err)
+	})
+
+	t.Run("unsupported file", func(t *testing.T) {
+		err := Copy("", "", 0, 0)
+
+		require.Equal(t, ErrUnsupportedFile, err)
+	})
+
+	t.Run("copy full", func(t *testing.T) {
+		var file *os.File
+		file, _ = os.Open(fromPath)
+		defer file.Close()
+
+		var fileInfo os.FileInfo
+		fileInfo, _ = file.Stat()
+
+		defer os.Remove(tempPath)
+
+		err := Copy(fromPath, tempPath, 0, 0)
+
+		var copiedFile *os.File
+		copiedFile, _ = os.Open(tempPath)
+		defer copiedFile.Close()
+
+		var copiedFileInfo os.FileInfo
+		copiedFileInfo, _ = copiedFile.Stat()
+
+		require.Equal(t, nil, err)
+		require.Equal(t, fileInfo.Size(), copiedFileInfo.Size())
+	})
+
+	t.Run("copy partial", func(t *testing.T) {
+		var file *os.File
+		file, _ = os.Open(fromPath)
+		defer file.Close()
+
+		var fileInfo os.FileInfo
+		fileInfo, _ = file.Stat()
+
+		fileSize := fileInfo.Size()
+		countToRead := fileSize / 4
+		offset := countToRead
+
+		defer os.Remove(tempPath)
+
+		err := Copy(fromPath, tempPath, offset, countToRead)
+
+		var copiedFile *os.File
+		copiedFile, _ = os.Open(tempPath)
+		defer copiedFile.Close()
+
+		var copiedFileInfo os.FileInfo
+		copiedFileInfo, _ = copiedFile.Stat()
+
+		require.Equal(t, nil, err)
+		require.Equal(t, countToRead, copiedFileInfo.Size())
+	})
+
+	t.Run("limit exceeds file size", func(t *testing.T) {
+		var file *os.File
+		file, _ = os.Open(fromPath)
+		defer file.Close()
+
+		var fileInfo os.FileInfo
+		fileInfo, _ = file.Stat()
+		fileSize := fileInfo.Size()
+
+		defer os.Remove(tempPath)
+
+		err := Copy(fromPath, tempPath, 0, (fileSize * 2))
+
+		var copiedFile *os.File
+		copiedFile, _ = os.Open(tempPath)
+		defer copiedFile.Close()
+
+		var copiedFileInfo os.FileInfo
+		copiedFileInfo, _ = copiedFile.Stat()
+
+		require.Equal(t, nil, err)
+		require.Equal(t, fileSize, copiedFileInfo.Size())
+	})
 }
