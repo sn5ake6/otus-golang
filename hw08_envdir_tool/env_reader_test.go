6f9fe01eed537d14e1704cbcc2f838c5fdb04321
Alexander Maslennikov <sn5ake6@gmail.com>
2022-05-29 23:28:51 +0500

    HW8 is completed

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,33 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("success dir read", func(t *testing.T) {
+		environments, err := ReadDir("testdata/env")
+
+		expected := Environment{
+			"BAR":   EnvValue{Value: "bar", NeedRemove: false},
+			"EMPTY": EnvValue{Value: "", NeedRemove: true},
+			"FOO":   EnvValue{Value: "   foo\nwith new line", NeedRemove: false},
+			"HELLO": EnvValue{Value: "\"hello\"", NeedRemove: false},
+			"UNSET": EnvValue{Value: "", NeedRemove: true},
+		}
+
+		require.Equal(t, expected, environments)
+		require.Nil(t, err)
+	})
+
+	t.Run("fail dir read", func(t *testing.T) {
+		environments, err := ReadDir("")
+
+		expected := Environment{}
+
+		require.Equal(t, expected, environments)
+		require.NotNil(t, err)
+	})
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,48 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("run with exit code 0", func(t *testing.T) {
+		cmd := []string{
+			"/bin/bash",
+			"testdata/echo.sh",
+		}
+		returnCode := RunCmd(cmd, Environment{})
+
+		require.Equal(t, 0, returnCode)
+	})
+
+	t.Run("run with exit code 2", func(t *testing.T) {
+		cmd := []string{
+			"/bin/bash",
+			"testdata/exit2.sh",
+		}
+		returnCode := RunCmd(cmd, Environment{})
+
+		require.Equal(t, 2, returnCode)
+	})
+
+	t.Run("run with not existed file", func(t *testing.T) {
+		cmd := []string{
+			"/bin/bash",
+			"testdata/echo_not_existed.sh",
+		}
+		returnCode := RunCmd(cmd, Environment{})
+
+		require.Equal(t, 127, returnCode)
+	})
+
+	t.Run("run without args", func(t *testing.T) {
+		cmd := []string{
+			"/bin/bash",
+		}
+		returnCode := RunCmd(cmd, Environment{})
+
+		require.Equal(t, 0, returnCode)
+	})
 }
