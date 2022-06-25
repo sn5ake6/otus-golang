a2cdb28557f0daa23c8c06520bb188a7c4e56c83
Alexander Maslennikov <sn5ake6@gmail.com>
2022-06-25 11:20:06 +0500

    HW11 is completed

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,21 +1,71 @@
 package main
 
 import (
+	"bufio"
 	"io"
+	"net"
 	"time"
 )
 
-type TelnetClient interface {
+type TelnetClienter interface {
 	Connect() error
-	io.Closer
+	Close() error
 	Send() error
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+type TelnetClient struct {
+	address string
+	timeout time.Duration
+	in      io.Reader
+	out     io.Writer
+	conn    net.Conn
+}
+
+func (cl *TelnetClient) Connect() error {
+	conn, err := net.DialTimeout("tcp", cl.address, cl.timeout)
+	if err != nil {
+		return err
+	}
+
+	cl.conn = conn
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (cl *TelnetClient) Receive() error {
+	return cl.transmit(cl.conn, cl.out)
+}
+
+func (cl *TelnetClient) Send() error {
+	return cl.transmit(cl.in, cl.conn)
+}
+
+func (cl *TelnetClient) transmit(from io.Reader, to io.Writer) error {
+	scanner := bufio.NewScanner(from)
+	for scanner.Scan() {
+		_, err := to.Write(append(scanner.Bytes(), '\n'))
+		if err != nil {
+			return err
+		}
+	}
+
+	return scanner.Err()
+}
+
+func (cl *TelnetClient) Close() error {
+	if cl.conn != nil {
+		return cl.conn.Close()
+	}
+
+	return nil
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClienter {
+	return &TelnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
